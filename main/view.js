6ff5225f4852c92849e48f7f1eb9dd4c1df40dc5
alan <ggddll123@qq.com>
2015-07-12 23:00:51 +0800

    update 2015/07/12

--- main/view.js
+++ main/view.js
@@ -16,7 +16,9 @@ define(function(require, exports, module){
 		ctor:function(data){
 			this.$super(data);
 			
-			this.$app = data.app;
+			this.$app = data.$app;
+			//可以动态设定$elem
+			this.$elem = data.$elem || this.$elem;
 			//共享网络和事件
 			this.$net = this.$app.$net;
 			//事件
@@ -30,15 +32,14 @@ define(function(require, exports, module){
 			if(this.events){
 				this.__bodyhandler = this.__bodyhandler || {};
 				for(var p in this.events){
+					var hasq = false;
 					for(var q in this.events[p]){
+						hasq = true;
 						this.$event.on(this,p,q,this.events[p][q]);
 					}
-					//绑定事件
-					if(!this.__bodyhandler[p]){
-						//绑定过的事件不再绑定
-						if(!this.__bodyhandler[p]){
-							this.__bodyhandler[p] = this.$event.bindEvent(this, this.$elem, p);
-						}
+					if(hasq){
+						//绑定事件
+						this.__bodyhandler[p] = this.$event.bindEvent(this, this.$elem, p);
 					}
 				}
 			}
@@ -53,6 +54,10 @@ define(function(require, exports, module){
 		destroy:function(){
 			//移除事件
 			this.$event.off(this);
+
+			for(var p in this.events){
+				this.$event.unbindEvent(this.$elem, p, this.__bodyhandler[p]);
+			}
 		}
 	})
 
